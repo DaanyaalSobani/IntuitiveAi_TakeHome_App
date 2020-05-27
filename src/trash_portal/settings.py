56ef118aa61a6883a58384062c2d41f885a34572
Daanyaal Sobani <daanyaal.sobani@gmail.com>
2020-05-26 17:12:03 -0700

    Overiding allauth templates

--- src/trash_portal/settings.py
+++ src/trash_portal/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'trash_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'shared/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +72,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
             ],
         },
     },
