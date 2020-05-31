e06ec6f76464280f11dece706dd45cb320426825
Daanyaal Sobani <daanyaal.sobani@gmail.com>
2020-05-30 22:35:37 -0700

    Last push

--- src/client_portal/models.py
+++ src/client_portal/models.py
@@ -8,6 +8,7 @@ class Organization(models.Model):
     def __str__(self):
         return self.name
 
+
 class WasteManager(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
--- src/trash_portal/settings.py
+++ src/trash_portal/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_extensions',
     'rest_framework',
     'client_portal',
     'staff_portal',
